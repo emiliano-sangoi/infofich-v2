3d947cf43d438bf384a9d6c03f10186299c01ece
Emiliano <emiliano.sangoi@gmail.com>
2020-02-26 20:46:43 -0300

    Nuevas dependencias: KnpPaginator y NelmioApiDocBundle. Se borraron dependencia inncesarias. Instalacion de api-infofich. Creacion de controller de ejemplo para probar la libreria api-infofich. Nuevas dependencias JS.

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,12 +5,29 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use FICH\APIRectorado\Config\WSHelper;
 
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
 
-    public function indexAction(Request $request)
-    {
+    public function indexAction(Request $request) {
         return $this->render('index.html.twig');
     }
+
+    public function pruebaAction(Request $request) {
+
+        $query = new \FICH\APIInfofich\Query\Carreras\QueryCarreras();
+
+        $data = $query
+                ->setUnidadAcademica(WSHelper::UA_FICH)
+                ->setTipoTitulo(WSHelper::TIPO_TITULO_GRADO)
+                ->setCacheEnabled(false)
+                ->getResultado();
+
+        dump($data, $query->getError());
+        exit;
+
+
+        return $this->render('index.html.twig');
+    }
+
 }
