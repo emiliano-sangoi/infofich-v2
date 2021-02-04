0b6fe85149b153a3214a88ac8bb4741eefce0f6b
Your Name <you@example.com>
2021-02-04 15:38:04 -0300

    cambios pedidos por marta

--- src/PlanificacionesBundle/Form/CargaHorariaType.php
+++ src/PlanificacionesBundle/Form/CargaHorariaType.php
@@ -14,7 +14,7 @@ class CargaHorariaType extends AbstractType {
     public function buildForm(FormBuilderInterface $builder, array $options) {
 //        getTotalCargaHorariaActividades
         $p = $options['planificacion'];
-        
+
         $builder
                 ->add('totalCargaHorariaAula', 'Symfony\Component\Form\Extension\Core\Type\NumberType', array(
                     'label' => 'Total',
@@ -22,9 +22,9 @@ class CargaHorariaType extends AbstractType {
                     'mapped' => false,
                     'data' => $p->getTotalCargaHorariaAula(),
                     'attr' => array('class' => 'form-control w-50', 'min' => 0, 'readonly' => true)
-                ));
-        
-        
+        ));
+
+
         $builder
                 ->add('totalCargaHorariaAutonomo', 'Symfony\Component\Form\Extension\Core\Type\IntegerType', array(
                     'label' => 'Estudio autónomo',
@@ -33,97 +33,114 @@ class CargaHorariaType extends AbstractType {
                     'data' => $p->getTotalCargaHorariaAutonomo(),
                     'attr' => array('class' => 'form-control w-50', 'min' => 0, 'readonly' => true)
         ));
-        
+
         $builder
                 ->add('totalFormacionPractica', 'Symfony\Component\Form\Extension\Core\Type\IntegerType', array(
-                    'label' => 'Formación práctica',
+                    'label' => 'Clase Práctica',
                     'required' => false,
                     'mapped' => false,
                     'data' => $p->getTotalFormacionPractica(),
                     'attr' => array('class' => 'form-control w-50', 'min' => 0, 'readonly' => true)
         ));
-        
+
         $builder
                 ->add('totalFormacionExperimental', 'Symfony\Component\Form\Extension\Core\Type\IntegerType', array(
-                    'label' => 'Formación experimental',
+                    'label' => 'Clase Teórico-Práctica',
                     'required' => false,
                     'mapped' => false,
                     'data' => $p->getTotalFormacionExperimental(),
                     'attr' => array('class' => 'form-control w-50', 'min' => 0, 'readonly' => true)
+                ))
+                ->add('cantHsResolProbIng', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                    //'label' => 'Resolución de problemas abiertos de ing',
+                    'label' => 'Otras actividades',
+                    'required' => false,
+                    'data' => $p->getTotalFormacionPractica(),
+                    'attr' => array(
+                        'class' => 'form-control w-50',
+                        'required' => false,
+                        'disabled' => true
+                    )
         ));
-        
-        
-        
-        
-        
-        $builder->add('cantHsResolProbIng', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Resolución de problemas abiertos de ing',
-                    'attr' => array(
-                        'class' => 'form-control w-25',
-                        'required' => true
-                    )
-                ))
-                ->add('cantHsEjRutinarios', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Ejercicios rutinarios',
-                    'attr' => array(
-                        'class' => 'form-control w-50',
-                        'required' => true
-                    )
-                ))
-                ->add('cantHsActProyDisenio', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Act. de proyecto y diseño',
-                    'attr' => array(
-                        'class' => 'form-control w-50',
-                        'required' => true
-                    )
-                ))
-                ->add('cantHsPracticaProfSup', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Práctica final supervisada',
-                    'attr' => array(
-                        'class' => 'form-control w-50',
-                        'required' => true
-                    )
-                ))
-                ->add('cantHsTeoria', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+        /*   ->add('cantHsEjRutinarios', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+          'label' => 'Ejercicios rutinarios',
+          'attr' => array(
+          'class' => 'form-control w-50',
+          'required' => true
+          )
+          )) */
+        /*   ->add('cantHsActProyDisenio', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+          'label' => 'Act. de proyecto y diseño',
+          'attr' => array(
+          'class' => 'form-control w-50',
+          'required' => true
+          )
+          )) */
+        /* ->add('cantHsPracticaProfSup', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+          'label' => 'Práctica final supervisada',
+          'attr' => array(
+          'class' => 'form-control w-50',
+          'required' => true
+          )
+          )) */
+        $builder->add('cantHsTeoria', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
                     'label' => 'Teoría',
+                    'required' => false,
+                    'data' => $p->getTotalFormacionPractica(),
                     'attr' => array(
                         'class' => 'form-control w-50',
-                        'required' => true
+                        'required' => true,
+                        'disabled' => true
                     )
                 ))
                 ->add('cantHsConsulta', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Evaluación',
+                    'label' => 'Consulta',
+                    'required' => false,
+                    'data' => $p->getTotalFormacionPractica(),
                     'attr' => array(
                         'class' => 'form-control w-50',
-                        'required' => true
+                        'required' => false,
+                        'disabled' => true
                     )
                 ))
                 ->add('cantHsEvaluacion', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
-                    'label' => 'Consultas',
+                    'label' => 'Evaluación',
+                    'required' => false,
+                    'data' => $p->getTotalFormacionPractica(),
                     'attr' => array(
                         'class' => 'form-control w-50',
-                        'required' => true
+                        'required' => false,
+                        'disabled' => true
+                    )
+                ))
+                ->add('cantHsPracticaProfSup', 'Symfony\Component\Form\Extension\Core\Type\TextType', array(
+                    'label' => 'Coloquio',
+                    'required' => false,
+                    'data' => $p->getTotalFormacionPractica(),
+                    'attr' => array(
+                        'class' => 'form-control w-50',
+                        'required' => false,
+                        'disabled' => true
                     )
         ));
-        $builder->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', array(
-            'attr' => array(
-                'class' => 'btn bg-verde text-color-white',
-                'onclick' => 'onGuardarDistribucionClick(event);'
-            ),
-            'label' => 'Guardar'
-        ));
+        /*   $builder->add('submit', 'Symfony\Component\Form\Extension\Core\Type\SubmitType', array(
+          'attr' => array(
+          'class' => 'btn bg-verde text-color-white',
+          'onclick' => 'onGuardarDistribucionClick(event);'
+          ),
+          'label' => 'Guardar'
+          ));
 
-        $builder->add('reset', 'Symfony\Component\Form\Extension\Core\Type\ResetType', array(
-            'label' => 'Limpiar campos',
-            'attr' => array('class' => 'btn btn-secondary')
-        ));
+          $builder->add('reset', 'Symfony\Component\Form\Extension\Core\Type\ResetType', array(
+          'label' => 'Limpiar campos',
+          'attr' => array('class' => 'btn btn-secondary')
+          )); */
         /* ->add('planificacion'); */
     }
 
-/**
+    /**
      * {@inheritdoc}
      */
-
     public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'data_class' => 'PlanificacionesBundle\Entity\CargaHoraria',
