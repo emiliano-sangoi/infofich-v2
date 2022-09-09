ff29bb8d259b5548ec59ba76c56db634dece3b7c
Romina Galarza <rominavgalarza@gmail.com>
2022-09-09 14:47:03 -0300

    Arreglos para Aprobacion asignatura

--- src/PlanificacionesBundle/Controller/AprobacionController.php
+++ src/PlanificacionesBundle/Controller/AprobacionController.php
@@ -23,9 +23,9 @@ class AprobacionController extends Controller
 
     use PlanificacionTrait;
 
-    public function editarUtilizaEvalContAction(Request $request, RequisitosAprobacion $requisitosAprob)
+    public function editarUtilizaEvalContAction(Request $request, Planificacion $planificacion)
     {
-        $planificacion = $requisitosAprob->getPlanificacion();
+        $requisitosAprob = $planificacion->getRequisitosAprobacion();
 
         $this->denyAccessUnlessGranted(Permisos::PLANIF_EDITAR, array('data' => $planificacion));
 
@@ -163,7 +163,7 @@ class AprobacionController extends Controller
 
         $fb->add('descEvalContinua', TextareaType::class, $opt);
 
-        return $fb->setAction($this->generateUrl('planif_aprobacion_actualizar_utiliza_ec', array('id' => $ra->getId())))
+        return $fb->setAction($this->generateUrl('planif_aprobacion_actualizar_utiliza_ec', array('id' => $ra->getPlanificacion())))
             ->setMethod('POST')->getForm();
     }
 
