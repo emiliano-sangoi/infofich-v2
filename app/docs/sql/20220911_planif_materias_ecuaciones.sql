809567b46ffe4a2964875df10f45ee5f402a5ee1
Brisa Basconcel <brisabasconcel@gmail.com>
2022-09-11 11:13:18 -0300

    Inicio un dump para estadisticas

---
+++ app/docs/sql/20220911_planif_materias_ecuaciones.sql
@@ -0,0 +1,23 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.10
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 18, 2022 at 04:26 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.2.34-32+0~20220627.74+debian11~1.gbpc7fa3c
+
+
+ALTER TABLE `planif_materias` ADD `recursantes` INT(11) NULL DEFAULT NULL ;
+
+--
+-- Dumping data for table `planif_materias`
+--
+
+INSERT INTO `planif_materias` (`id`, `carrera`, `plan`, `version_plan`, `codigo_materia`, `nombre_materia`, 
+`tipo_materia`, `hs_semanales`, `hs_carga_horaria`, `hs_valor_materia`, `promediable`, `obligatoria`, `anio_cursada`, `periodo_cursada`, `tipo_cursada`, `nro_modulo`) VALUES
+(NULL, '1069','012006', 1, 'FCA10009','Estadística','N', NULL, '30.00', NULL, 1, 1, 1, '1er Cuatrimestre', 'cuatrimestre', 1);
+(NULL, '1071','012006', 1, 'FCA10009','Estadística','N', NULL, '30.00', NULL, 1, 1, 1, '1er Cuatrimestre', 'cuatrimestre', 1);
+(NULL, '1077','082005', 1, 'FCA10009','Estadística','N', NULL, '30.00', NULL, 1, 1, 1, '1er Cuatrimestre', 'cuatrimestre', 1);
+
+-- faltan los datos que proveerá Leopoldo desde 'Estadística'
