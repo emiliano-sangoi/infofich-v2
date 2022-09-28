23a2bdcf67500bfd7c0cbb00289776c2dce613d7
Romina Galarza <rominavgalarza@gmail.com>
2022-09-28 15:07:48 -0300

    Agregamos update de planes en tabla planificaciones que estaban mal

---
+++ app/docs/sql/20220928_planif_planificaciones_update.sql
@@ -0,0 +1,41 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.10
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost
+-- Generation Time: Aug 18, 2022 at 04:26 PM
+-- Server version: 8.0.27
+-- PHP Version: 7.2.34-32+0~20220627.74+debian11~1.gbpc7fa3c
+
+
+--
+-- Updates data for table `planif_planificaciones`
+--
+
+UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 60;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 61; 
+UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 62;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 63;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 65;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 66;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 75;
+UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 77;
+-- UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 79;
+UPDATE `planif_planificaciones` SET `plan` = '032006' WHERE `planif_planificaciones`.`id` = 80;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 83;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 84;
+
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 92;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 94;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 95;
+
+UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 96;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 98;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 103;
+
+-- UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 106;
+-- UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 107;
+UPDATE `planif_planificaciones` SET `plan` = '062007' WHERE `planif_planificaciones`.`id` = 109;
+UPDATE `planif_planificaciones` SET `plan` = '012006' WHERE `planif_planificaciones`.`id` = 110;
+UPDATE `planif_planificaciones` SET `plan` = '022006' WHERE `planif_planificaciones`.`id` = 112;
+UPDATE `planif_planificaciones` SET `plan` = '082005' WHERE `planif_planificaciones`.`id` = 113;
