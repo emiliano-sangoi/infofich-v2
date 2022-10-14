e8b89c4ada9dbf5cfdddaf24f505e5e3b0d1d863
Romina Galarza <rominavgalarza@gmail.com>
2022-10-14 20:15:42 -0300

    correccion en correccion sql recursantes

--- app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
+++ app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
@@ -13,3 +13,7 @@ update planif_asignaturas set carrera_id = 21 where carrera_id = 17;
 -- IRH -> Se cambia el ID 8 (plan 022004) por ID 9 (plan 2006):
 update planif_asignaturas set carrera_id = 9 where carrera_id = 8;
 
+-- se eliminan por estar repetidas
+delete from planif_asignaturas
+where carrera_id = 11 and
+        id in (795, 856, 845, 855, 829, 858, 807, 962, 821, 812, 811, 810, 846, 834, 841, 826, 796, 847, 824, 825, 806, 808, 929, 859, 853, 863, 822, 816,848, 849,813, 850, 836, 887, 891, 814, 823, 854, 862 );
