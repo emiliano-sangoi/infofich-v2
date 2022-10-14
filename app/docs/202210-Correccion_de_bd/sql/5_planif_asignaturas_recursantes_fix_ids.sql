07c51e9f3ed44cf0bc0d3e2229125d7f0a60b5f7
Romina Galarza <rominavgalarza@gmail.com>
2022-10-14 19:46:17 -0300

    correccion en correccion sql recursantes

--- app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
+++ app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
@@ -6,7 +6,9 @@ update planif_asignaturas set es_recursada = 0 where es_recursada IS NULL;
 update planif_asignaturas set carrera_id = 11 where carrera_id = 10;
 
 -- Se borra aquellas vinculadas a ANALISTA EN INFORMATICA
-delete from planif_asignaturas where carrera_id = 17;
+-- ID DE ANALISTA EN INFORMATICA ERA PARA ING AGRIMENSURA
+update planif_asignaturas set carrera_id = 21 where carrera_id = 17;
+
 
 -- IRH -> Se cambia el ID 8 (plan 022004) por ID 9 (plan 2006):
 update planif_asignaturas set carrera_id = 9 where carrera_id = 8;
