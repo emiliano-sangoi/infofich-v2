01dbb738f531954a61f1f6e70d181f76bdde0711
Romina Galarza <rominavgalarza@gmail.com>
2022-10-14 19:55:56 -0300

    correccion en correccion sql recursantes

--- app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
+++ app/docs/202210-Correccion_de_bd/sql/5_planif_asignaturas_recursantes_fix_ids.sql
@@ -12,19 +12,3 @@ update planif_asignaturas set carrera_id = 21 where carrera_id = 17;
 
 -- IRH -> Se cambia el ID 8 (plan 022004) por ID 9 (plan 2006):
 update planif_asignaturas set carrera_id = 9 where carrera_id = 8;
-
-INSERT INTO `planif_asignaturas`
-(`id`, `carrera_id`, `codigo_asignatura` , `nombre_asignatura`,
- `tipo_asignatura`, `hs_semanales`, `hs_carga_horaria`, `hs_valor_asignatura`,
- `promediable`, `obligatoria`, `anio_cursada`, `periodo_cursada`,
- `tipo_cursada`, `nro_modulo`,  `es_recursada`, `origen_ws`,`fecha_actualizacion`)
-VALUES
-(NULL, 21, 'FICHIRH01', 'MATEMÁTICA BÁSICA (RECURSANTES)',
- 'N', NULL, '90.00', NULL,
- 1, 1, 1, '2do Cuatrimestre',
- 'cuatrimestre', NULL,1, false, NOW()),
-(NULL, 21, 'FICHIF003', 'QUÍMICA GENERAL (RECURSANTES)',
- 'N', NULL, '60.00', NULL,
- 1, 1, 1, '2do Cuatrimestre',
- 'cuatrimestre', NULL, 1, false, NOW());
-
