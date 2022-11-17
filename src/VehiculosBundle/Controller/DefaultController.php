02c7688451244f3619fe87c67d033871c0f4f325
Romina Galarza <rominavgalarza@gmail.com>
2022-11-17 16:08:40 -0300

    Se generan archivos para bundle vehiculos

--- src/VehiculosBundle/Controller/DefaultController.php
+++ src/VehiculosBundle/Controller/DefaultController.php
@@ -8,6 +8,32 @@ class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('VehiculosBundle:Default:index.html.twig');
+        
+           // Breadcrumbs
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
+
+
+        return $this->render('VehiculosBundle:Default:index.html.twig', array(
+            'page_title' => 'InfoFICH - Vehículos',
+            //s'docentes_paginado' => $docentes_paginado
+        ));
+ 
+    }
+    public function listadoAction()
+    {
+        
+           // Breadcrumbs
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
+
+
+        return $this->render('VehiculosBundle:Default:listado.html.twig', array(
+            'page_title' => 'InfoFICH - Vehículos',
+            //s'docentes_paginado' => $docentes_paginado
+        ));
+ 
     }
 }
