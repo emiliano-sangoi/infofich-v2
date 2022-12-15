6ca88be169a443ffd8c13aca8a8feac5c1286962
Brisa Basconcel <brisabasconcel@gmail.com>
2022-12-15 19:51:35 -0300

    Se agrega la entidad Vehiculo con getter y setter

--- src/VehiculosBundle/Entity/Vehiculo.php
+++ src/VehiculosBundle/Entity/Vehiculo.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Entity;
+namespace VehiculosBundle\Entity;
 
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
@@ -19,108 +19,278 @@ class Vehiculo
      * @ORM\Id
      * @ORM\GeneratedValue(strategy="AUTO")
      */
-
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $vechicleTitle;
+    private $tipo;
 
     /**
      * @ORM\Column(type="string", length=255)
      */
-    private $VechilcleOverview;
+    private $patente;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $descripcion;
 
     /**
      * @ORM\Column(type="integer")
      */
-    private $PrricePerDay;
+    private $asociado;
 
     /**
      * @ORM\Column(type="string", length=50)
      */
-    private $FuelType;
+    private $combustible;
+    
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $marca;
 
     /**
      * @ORM\Column(type="date")
      */
-    private $ModelYear;
+    private $modelo;
 
     /**
      * @ORM\Column(type="integer")
      */
-    private $SeatingCapacity;
-
-    /**
-     * @ORM\Column(type="string", length=50, nullable=true)
-     */
-    private $Vimage1;
-
-    /**
-     * @ORM\Column(type="string", length=50, nullable=true)
-     */
-    private $Vimage2;
-
-    /**
-     * @ORM\Column(type="string", length=50, nullable=true)
-     */
-    private $Vimage;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $AirConditioner;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $PowerDoorLocks;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $AntiLockBraking;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $BreakAssist;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $PowerSteering;
-
-    /**
-     * @ORM\Column(type="boolean", nullable=true)
-     */
-    private $DriverAirBag;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     */
-    private $regDate;
-
-    /**
-     * @ORM\Column(type="date", nullable=true)
-     */
-    private $UpdationDate;
-
-    /**
-     * @ORM\ManyToOne(targetEntity="App\Entity\Brands",inversedBy="Vechicles")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $brands;
-
-    /**
-     * @ORM\ManyToMany(targetEntity="App\Entity\Membres", mappedBy="vechicles")
-     */
-    private $membres;
-
-    /**
-     * @ORM\OneToOne(targetEntity="App\Entity\Booking", mappedBy="vechicles", cascade={"persist", "remove"})
-     */
-    private $booking;
+    private $capacidad;
     
+    /**
+     * @ORM\Column(type="boolean", nullable=true)
+     */
+    private $activo;
+
+    
+
+    /**
+     * Get id
+     *
+     * @return integer
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set tipo
+     *
+     * @param string $tipo
+     *
+     * @return Vehiculo
+     */
+    public function setTipo($tipo)
+    {
+        $this->tipo = $tipo;
+
+        return $this;
+    }
+
+    /**
+     * Get tipo
+     *
+     * @return string
+     */
+    public function getTipo()
+    {
+        return $this->tipo;
+    }
+
+    /**
+     * Set patente
+     *
+     * @param string $patente
+     *
+     * @return Vehiculo
+     */
+    public function setPatente($patente)
+    {
+        $this->patente = $patente;
+
+        return $this;
+    }
+
+    /**
+     * Get patente
+     *
+     * @return string
+     */
+    public function getPatente()
+    {
+        return $this->patente;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return Vehiculo
+     */
+    public function setDescripcion($descripcion)
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion()
+    {
+        return $this->descripcion;
+    }
+
+    /**
+     * Set asociado
+     *
+     * @param integer $asociado
+     *
+     * @return Vehiculo
+     */
+    public function setAsociado($asociado)
+    {
+        $this->asociado = $asociado;
+
+        return $this;
+    }
+
+    /**
+     * Get asociado
+     *
+     * @return integer
+     */
+    public function getAsociado()
+    {
+        return $this->asociado;
+    }
+
+    /**
+     * Set combustible
+     *
+     * @param string $combustible
+     *
+     * @return Vehiculo
+     */
+    public function setCombustible($combustible)
+    {
+        $this->combustible = $combustible;
+
+        return $this;
+    }
+
+    /**
+     * Get combustible
+     *
+     * @return string
+     */
+    public function getCombustible()
+    {
+        return $this->combustible;
+    }
+
+    /**
+     * Set marca
+     *
+     * @param string $marca
+     *
+     * @return Vehiculo
+     */
+    public function setMarca($marca)
+    {
+        $this->marca = $marca;
+
+        return $this;
+    }
+
+    /**
+     * Get marca
+     *
+     * @return string
+     */
+    public function getMarca()
+    {
+        return $this->marca;
+    }
+
+    /**
+     * Set modelo
+     *
+     * @param \DateTime $modelo
+     *
+     * @return Vehiculo
+     */
+    public function setModelo($modelo)
+    {
+        $this->modelo = $modelo;
+
+        return $this;
+    }
+
+    /**
+     * Get modelo
+     *
+     * @return \DateTime
+     */
+    public function getModelo()
+    {
+        return $this->modelo;
+    }
+
+    /**
+     * Set capacidad
+     *
+     * @param integer $capacidad
+     *
+     * @return Vehiculo
+     */
+    public function setCapacidad($capacidad)
+    {
+        $this->capacidad = $capacidad;
+
+        return $this;
+    }
+
+    /**
+     * Get capacidad
+     *
+     * @return integer
+     */
+    public function getCapacidad()
+    {
+        return $this->capacidad;
+    }
+
+    /**
+     * Set activo
+     *
+     * @param boolean $activo
+     *
+     * @return Vehiculo
+     */
+    public function setActivo($activo)
+    {
+        $this->activo = $activo;
+
+        return $this;
+    }
+
+    /**
+     * Get activo
+     *
+     * @return boolean
+     */
+    public function getActivo()
+    {
+        return $this->activo;
+    }
 }
