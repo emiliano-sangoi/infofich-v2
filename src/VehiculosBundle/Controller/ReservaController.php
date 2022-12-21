78287c60e6b20be0d5c65b5fae6a061a322237db
Romina Galarza <rominavgalarza@gmail.com>
2022-12-21 15:24:37 -0300

    routing reservas

---
+++ src/VehiculosBundle/Controller/ReservaController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace VehiculosBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use VehiculosBundle\Entity\Vehiculo;
+
+
+class VehiculoController extends Controller {
+
+   
+
+    public function listadoAction() {
+
+        // Breadcrumbs
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
+
+
+        return $this->render('VehiculosBundle:Reserva:listado.html.twig', array(
+                    'page_title' => 'InfoFICH - Reserva Vehículos',
+                        //s'docentes_paginado' => $docentes_paginado
+        ));
+    }
+
+}
