3d618104eeef6ff3ddb903b7117741769195f268
Romina Galarza <rominavgalarza@gmail.com>
2022-12-21 12:22:37 -0300

    agrego vehiculos nuevo

--- src/VehiculosBundle/Controller/DefaultController.php
+++ src/VehiculosBundle/Controller/DefaultController.php
@@ -3,37 +3,76 @@
 namespace VehiculosBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 
-class DefaultController extends Controller
-{
-    public function indexAction()
-    {
-        
-           // Breadcrumbs
+
+class DefaultController extends Controller {
+
+    public function indexAction() {
+
+        // Breadcrumbs
         $breadcrumbs = $this->get("white_october_breadcrumbs");
         $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
         $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
 
 
         return $this->render('VehiculosBundle:Default:index.html.twig', array(
-            'page_title' => 'InfoFICH - Vehículos',
-            //s'docentes_paginado' => $docentes_paginado
+                    'page_title' => 'InfoFICH - Vehículos',
+                        //s'docentes_paginado' => $docentes_paginado
         ));
- 
     }
-    public function listadoAction()
-    {
-        
-           // Breadcrumbs
+
+    public function listadoAction() {
+
+        // Breadcrumbs
         $breadcrumbs = $this->get("white_october_breadcrumbs");
         $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
         $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
 
 
         return $this->render('VehiculosBundle:Default:listado.html.twig', array(
-            'page_title' => 'InfoFICH - Vehículos',
-            //s'docentes_paginado' => $docentes_paginado
+                    'page_title' => 'InfoFICH - Vehículos',
+                        //s'docentes_paginado' => $docentes_paginado
         ));
- 
     }
+
+    public function newAction(Request $request) {
+
+
+        //$docenteAdscripto = new DocenteAdscripto();
+        //$form = $this->createForm(DocenteAdscriptoType::class, $docenteAdscripto);
+        //    $form->handleRequest($request);
+
+        /*  if ($form->isSubmitted() && $form->isValid()) {
+
+          $em = $this->getDoctrine()->getManager();
+
+          if ($docenteAdscripto->getPersona()->getId()) {
+          $persona = $em->merge($docenteAdscripto->getPersona());
+          $docenteAdscripto->setPersona($persona);
+          }
+
+          $em->persist($docenteAdscripto);
+          $em->flush();
+
+          $this->addFlash('success', 'Docente adscripto creado correctamente');
+
+          return $this->redirectToRoute('docentes_adscriptos');
+
+          //return $this->redirectToRoute('docentes_adscriptos_show', array('id' => $docenteAdscripto->getId()));
+          } */
+
+        // Breadcrumbs
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
+        $breadcrumbs->addItem("Nuevo");
+
+        return $this->render('VehiculosBundle:Default:new.html.twig', array(
+                    //'docenteAdscripto' => $docenteAdscripto,
+                   // 'form' => $form->createView(),
+                    'page_title' => 'Vehiculos - Nuevo',
+        ));
+    }
+
 }
