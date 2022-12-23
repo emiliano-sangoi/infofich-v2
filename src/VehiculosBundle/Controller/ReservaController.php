ef9f6fd1563200562a2467ea4c9a67f2b0861bcf
Romina Galarza <rominavgalarza@gmail.com>
2022-12-23 11:45:46 -0300

    avances en pantallas de reserva vehiculos

--- src/VehiculosBundle/Controller/ReservaController.php
+++ src/VehiculosBundle/Controller/ReservaController.php
@@ -16,12 +16,51 @@ class ReservaController extends Controller {
         // Breadcrumbs
         $breadcrumbs = $this->get("white_october_breadcrumbs");
         $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
-        $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
+        $breadcrumbs->addItem("Vehículos / Reservas", $this->get("router")->generate("vehiculos_index"));
 
 
         return $this->render('VehiculosBundle:Reserva:listado.html.twig', array(
                     'page_title' => 'InfoFICH - Reserva Vehículos',
                         //s'docentes_paginado' => $docentes_paginado
+        ));
+    }
+    
+     public function newAction(Request $request) {
+
+        //TODO DESCOMENTAR Y COMPLETAR CUANDO TENGAMOS LA BD
+        //$docenteAdscripto = new DocenteAdscripto();
+        //$form = $this->createForm(DocenteAdscriptoType::class, $docenteAdscripto);
+        //    $form->handleRequest($request);
+
+        /*  if ($form->isSubmitted() && $form->isValid()) {
+
+          $em = $this->getDoctrine()->getManager();
+
+          if ($docenteAdscripto->getPersona()->getId()) {
+          $persona = $em->merge($docenteAdscripto->getPersona());
+          $docenteAdscripto->setPersona($persona);
+          }
+
+          $em->persist($docenteAdscripto);
+          $em->flush();
+
+          $this->addFlash('success', 'Docente adscripto creado correctamente');
+
+          return $this->redirectToRoute('docentes_adscriptos');
+
+          //return $this->redirectToRoute('docentes_adscriptos_show', array('id' => $docenteAdscripto->getId()));
+          } */
+
+        // Breadcrumbs
+        $breadcrumbs = $this->get("white_october_breadcrumbs");
+        $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
+        $breadcrumbs->addItem("Vehículos / Reservas", $this->get("router")->generate("reservas_listado"));
+        $breadcrumbs->addItem("Nuevo");
+
+        return $this->render('VehiculosBundle:Reserva:new.html.twig', array(
+                    //'docenteAdscripto' => $docenteAdscripto,
+                   // 'form' => $form->createView(),
+                    'page_title' => 'Reserva - Nueva',
         ));
     }
 
