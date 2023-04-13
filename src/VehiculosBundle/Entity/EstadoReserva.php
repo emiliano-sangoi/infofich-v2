bf476d54290ea341638d3b54c24f681cac8acfe3
Brisa Basconcel <brisabasconcel@gmail.com>
2023-04-13 20:30:24 -0300

    agergo estado de la reserva

---
+++ src/VehiculosBundle/Entity/EstadoReserva.php
@@ -0,0 +1,187 @@
+<?php
+
+namespace VehiculosBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Estado de la reserva
+ *
+ * @ORM\Table(name="vehiculo_reserva_estado")")
+ * @ORM\Entity(repositoryClass="VehiculosBundle\Repository\EstadoRepository")
+ */
+class EstadoReserva implements \JsonSerializable{
+
+    const NUEVA = 1;
+    const ACEPTADA = 2;
+    const RECHAZADA = 3;
+    const CANCELADA = 4;
+
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+    
+    /**
+     * @var smallint
+     *
+     * @ORM\Column(name="codigo", type="smallint", unique=true)
+     */
+    private $codigo;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="nombre", type="string", length=64)
+     */
+    private $nombre;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="descripcion", type="string", length=512, nullable=true)
+     */
+    private $descripcion;
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId() {
+        return $this->id;
+    }
+    
+    public function __toString() {
+        return $this->nombre;
+    }
+
+    
+    public function getCssClass() {
+        switch ($this->codigo){
+            case self::NUEVA:
+                return 'light';
+            case self::CANCELADA:
+                return 'warning';
+            case self::RECHAZADA:
+                return 'danger';
+            case self::ACEPTADA:
+                return 'success';
+        }
+        
+        return '';
+    }
+    
+    /**
+     * Verifica si el estado pasado como parametro es valido.
+     * 
+     * @param type $cod
+     * @return type
+     */
+    public static function isValido($cod) {
+        return in_array($cod, array(
+            self::NUEVA,
+            self::ACEPTADA,
+            self::CANCELADA,
+            self::RECHAZADA
+        ));
+    }
+
+    public static function getNombrePorCod($cod) {
+        switch ($cod) {
+            case self::NUEVA:
+                return 'Nueva';
+            case self::ACEPTADA:
+                return 'Aceptada';
+            case self::CANCELADA:
+                return 'Cancelada';
+            case self::RECHAZADA:
+                return 'Rechazada';
+            default:
+                return false;
+        }
+    }
+
+    /**
+     * Set nombre
+     *
+     * @param string $nombre
+     *
+     * @return Estado
+     */
+    public function setNombre($nombre) {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    /**
+     * Get nombre
+     *
+     * @return string
+     */
+    public function getNombre() {
+        return $this->nombre;
+    }
+
+    /**
+     * Set descripcion
+     *
+     * @param string $descripcion
+     *
+     * @return Estado
+     */
+    public function setDescripcion($descripcion) {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+
+    /**
+     * Get descripcion
+     *
+     * @return string
+     */
+    public function getDescripcion() {
+        return $this->descripcion;
+    }
+
+
+    /**
+     * Set codigo
+     *
+     * @param integer $codigo
+     *
+     * @return Estado
+     */
+    public function setCodigo($codigo)
+    {
+        $this->codigo = $codigo;
+
+        return $this;
+    }
+
+    /**
+     * Get codigo
+     *
+     * @return integer
+     */
+    public function getCodigo()
+    {
+        return $this->codigo;
+    }
+
+    public function jsonSerialize() {
+        return array(
+            'id' => $this->id,
+            'codigo' => $this->codigo,
+            'nombre' => $this->nombre,
+            'descripcion' => $this->descripcion,
+        );
+    }
+
+}
---
+++ src/VehiculosBundle/Repository/EstadoRepository.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace VehiculosBundle\Repository;
+
+/**
+ * EstadoRepository
+ *
+ */
+class EstadoRepository extends \Doctrine\ORM\EntityRepository
+{
+}
