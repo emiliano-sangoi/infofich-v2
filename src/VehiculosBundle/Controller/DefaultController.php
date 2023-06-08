abb5659970a802c1e7e5f95e65091f309ec1e12c
Romina Galarza <rgalarza@santafe.gov.ar>
2023-06-08 09:22:30 -0300

    mas sobre permisos

--- src/VehiculosBundle/Controller/DefaultController.php
+++ src/VehiculosBundle/Controller/DefaultController.php
@@ -16,7 +16,6 @@ class DefaultController extends Controller {
         $breadcrumbs->addItem("Inicio", $this->get("router")->generate("homepage"));
         $breadcrumbs->addItem("Vehículos", $this->get("router")->generate("vehiculos_index"));
 
-
         return $this->render('VehiculosBundle:Default:index.html.twig', array(
                     'page_title' => 'InfoFICH - Vehículos',
                         //s'docentes_paginado' => $docentes_paginado
